434820b7fef56f972564ec55d9002872a32838c0
Ilya Smelkov <ismelkov@restream.rt.ru>
2016-04-29 11:38:18 +0300

    Monadic tokenizer

--- src/Youtan/Lexical/Tokenizer.hs
+++ src/Youtan/Lexical/Tokenizer.hs
@@ -1,13 +1,17 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 -- | Parses strings into a set of tokens using the given rules.
 module Youtan.Lexical.Tokenizer
 ( tokenize
+, tokenizeT
 , Rules
 ) where
 
 import Control.Monad.State
+import Control.Monad.Except
 import Data.List ( inits )
 
-import Youtan.Regex.DFM ( match )
+import Youtan.Regex.NDFM ( match )
 
 -- | Represents a set of parsing rules.
 type Rules a = [ ( String, String -> a ) ]
@@ -15,24 +19,35 @@ type Rules a = [ ( String, String -> a ) ]
 -- | Applies a set of parsing rules to split the input string
 -- into a set of tokens.
 tokenize :: MonadPlus m => Rules a -> String -> m a
-tokenize rules = evalState ( parse mzero rules )
+tokenize r s = either error id ( tokenizeT r s )
+  -- evalState ( parse mzero rules )
+
+-- | Monadic version of 'tokenize' wrappers errors into a monad.
+tokenizeT :: ( Wrapper w, MonadPlus m ) => Rules a -> String -> w ( m a )
+tokenizeT rules = evalStateT ( parse mzero rules )
 
 -- | Shortcut for types.
-type P a = State String a
+type P w a = StateT String w a
+
+-- | Shortcut for monad error.
+type Wrapper w = MonadError String w
 
 -- | Applies a regex to an input and returns the longest match.
-tryParse :: String -> P ( Maybe String )
+tryParse :: Wrapper w => String -> P w ( Maybe String )
 tryParse regex = do
   inp <- get
+  -- TODO: Need another function, that would stop matching as soon as
+  -- DFM starts failing.
+  -- E.g. for regex "a*" no reason to try all inits for "aab... 1K chars more".
   let x = filter ( match regex ) ( inits inp )
       y = last x
-  if null x
+  if null x || null y
   then return Nothing
   else modify ( drop ( length y ) ) >> return ( Just y )
 
 -- | Applies rules one by one until one matches or
-choice :: Rules a -> P a
-choice [] = error "All options failed"
+choice :: Wrapper w => Rules a -> P w a
+choice [] = ( take 10 <$> get ) >>= throwError . (++) "All options failed "
 choice ( ( r, f ) : xs ) = do
   x <- tryParse r
   case x of
@@ -40,11 +55,11 @@ choice ( ( r, f ) : xs ) = do
     Nothing -> choice xs
 
 -- | Checks if the whole input is consumed.
-done :: P Bool
+done :: Wrapper w => P w Bool
 done = null <$> get
 
 -- | Splits the input into a set of tokens.
-parse :: MonadPlus m => m a -> Rules a -> P ( m a )
+parse :: ( MonadPlus m, Wrapper w ) => m a -> Rules a -> P w ( m a )
 parse stack rules = do
   status <- done
   if status
--- test/Youtan/Lexical/TokenizerTest.hs
+++ test/Youtan/Lexical/TokenizerTest.hs
@@ -2,6 +2,8 @@ module Youtan.Lexical.TokenizerTest where
 
 import Test.Hspec
 
+import Control.Exception ( evaluate )
+
 import Youtan.Lexical.Tokenizer
 
 data Oper = Plus | Minus
@@ -20,13 +22,82 @@ algRules = [ ( "\\+", const ( Action Plus ) )
            , ( "[^+-\\d]+", Rest )
            ]
 
+data JavaToken
+  = Comment String
+  | Keyword String
+  | Brace String
+  | Dot
+  | Colon
+  | Space
+  | Iden String
+  | Str String
+  deriving ( Show, Eq )
+
+javaHelloWorld :: String
+javaHelloWorld = unlines
+  [ "/* HelloWorld.java"
+  , "*/"
+  , ""
+  , "public class HelloWorld"
+  , "{"
+  , "\tpublic static void main(String[] args) {"
+  , "\t\tSystem.out.println(\"Hello World!\");"
+  , "\t}"
+  , "}"
+  ]
+
+javaRules :: Rules JavaToken
+javaRules =
+  [ ( "\\/\\*.*\\*/", Comment )
+  , ( "(public|class|static|void)", Keyword )
+  , ( "[{}()\\[\\]]", Brace )
+  , ( "\\.", const Dot )
+  , ( ";", const Colon )
+  , ( "\\\"[^\\\"]*\\\"", Str )
+  , ( "\\s+", const Space )
+  , ( "[a-zA-Z][0-9a-zA-Z]*", Iden )
+  ]
+
+dropSpaces :: [ JavaToken ] -> [ JavaToken ]
+dropSpaces [] = []
+dropSpaces ( Space : xs ) = dropSpaces xs
+dropSpaces ( x : xs ) = x : dropSpaces xs
+
+javaHelloWorldTokens :: [ JavaToken ]
+javaHelloWorldTokens =
+  [ Comment "/* HelloWorld.java\n*/"
+  , Keyword "public", Keyword "class", Iden "HelloWorld"
+  , Brace "{", Keyword "public", Keyword "static", Keyword "void", Iden "main", Brace "(", Iden "String", Brace "[", Brace "]",Iden "args",Brace ")",Brace "{"
+  , Iden "System",Dot,Iden "out",Dot,Iden "println",Brace "(",Str "\"Hello World!\"",Brace ")",Colon
+  , Brace "}"
+  , Brace "}"
+  ]
+
 spec :: SpecWith ()
 spec = context "parseString" $ do
-  context "AlgExpression" $ do
+  context "tokenize" $ do
     it "Empty string" $
       tokenize algRules "" `shouldBe` []
     it "Few tokens" $
       tokenize algRules "12+34" `shouldBe` [ Num 12, Action Plus, Num 34 ]
     it "Collects unknown chars" $
       tokenize algRules "12abc+34" `shouldBe` [ Num 12, Rest "abc", Action Plus, Num 34 ]
+    it "Fails with no parse" $
+      evaluate ( tokenize [ ( "-", id ) ] "+" :: [ String ] ) `shouldThrow` anyException
+    it "Ignores wildchars" $
+      tokenize [ ( "a*", id ), ( "-", id ) ] "-" `shouldBe` [ "-" ]
+    it "" $
+      dropSpaces ( tokenize javaRules javaHelloWorld ) `shouldBe` javaHelloWorldTokens
 
+  context "tokenizeT" $ do
+    it "Empty string" $
+      tokenizeT algRules "" `shouldBe` Right []
+    it "Few tokens" $
+      tokenizeT algRules "12+34" `shouldBe` Right [ Num 12, Action Plus, Num 34 ]
+    it "Collects unknown chars" $
+      tokenizeT algRules "12abc+34" `shouldBe` Right [ Num 12, Rest "abc", Action Plus, Num 34 ]
+    it "Fails with no parse" $
+      tokenizeT [ ( "-", id ) ] "+" `shouldBe`
+        ( Left "All options failed +" :: Either String [ String ] )
+    it "Ignores wildchars" $
+      tokenizeT [ ( "a*", id ), ( "-", id ) ] "-" `shouldBe` Right [ "-" ]
