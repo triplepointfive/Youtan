22154f85162a556beb50019dd16a0439d0bc0004
Ilya Smelkov <triplepointfive@gmail.com>
2016-06-04 21:03:22 +0300

    Drop CFG

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,7 +4,6 @@ import qualified Youtan.Regex.DFMTest as DFM ( spec )
 import qualified Youtan.Regex.NDFMTest as NDFM ( spec )
 import qualified Youtan.Regex.OperatorsTest as Operators ( spec )
 import qualified Youtan.Lexical.TokenizerTest as Tokenizer ( spec )
-import qualified Youtan.Syntax.CFGTest as CFG ( spec )
 import qualified Parser.JSONTest as JSON ( spec )
 
 main :: IO ()
@@ -13,7 +12,6 @@ main = hspec $ parallel $ do
   describe "NDFM" NDFM.spec
   describe "Operators" Operators.spec
   describe "Tokenizer" Tokenizer.spec
-  describe "CFG" CFG.spec
 
   describe "examples" $ do
     describe "JSON" JSON.spec
