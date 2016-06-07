a46ac279f93d41a7f6536b08bcfb649391ba2623
Ilya Smelkov <ismelkov@restream.rt.ru>
2016-06-07 11:39:30 +0300

    Syntax parser

--- examples/BF/Main.hs
+++ examples/BF/Main.hs
@@ -4,15 +4,18 @@ import           Control.Monad.State
 import           Data.Char ( chr, ord )
 import qualified Data.Sequence as Seq
 
-import Youtan.Lexical.Tokenizer
+import           Youtan.Lexical.Tokenizer ( Rules, tokenizeDrops )
+import           Youtan.Syntax.Parser ( term, (<<), many, runParser, Parser )
+
+-- Lexical.
 
 data Token
   = IncrP
   | DecrP
   | IncrB
   | DecrB
-  | Output
-  | Input
+  | Out
+  | In
   | Begin
   | End
   deriving ( Eq )
@@ -22,66 +25,110 @@ instance Show Token where
   show DecrP  = "<"
   show IncrB  = "+"
   show DecrB  = "-"
-  show Output = "."
-  show Input  = ","
+  show Out    = "."
+  show In     = ","
   show Begin  = "["
   show End    = "]"
 
-rules :: Rules Token
-rules =
-  [ ( ">", const IncrP )
-  , ( "<", const DecrP )
-  , ( "\\+", const IncrB )
-  , ( "-", const DecrB )
-  , ( "\\.", const Output )
-  , ( ",", const Input )
-  , ( "\\[", const Begin )
-  , ( "\\]", const End )
-  ]
-
-drops :: [ String ]
-drops = [ "[^-\\[\\]\\.\\,><\\+]+" ]
-
 lexical :: String -> [ Token ]
 lexical = tokenizeDrops rules drops
+  where
+    rules :: Rules Token
+    rules =
+      [ ( ">", const IncrP )
+      , ( "<", const DecrP )
+      , ( "\\+", const IncrB )
+      , ( "-", const DecrB )
+      , ( "\\.", const Out )
+      , ( ",", const In )
+      , ( "\\[", const Begin )
+      , ( "\\]", const End )
+      ]
+    drops :: [ String ]
+    drops = [ "[^-\\[\\]\\.\\,><\\+]+" ]
 
-type Cell = Int
+-- Syntax.
+
+data Action
+  = IncrValue
+  | DecrValue
+  | MoveForth
+  | MoveBack
+  | Output
+  | Input
+  | Loop [ Action ]
+  deriving ( Eq )
+
+instance Show Action where
+  show IncrValue        = ">"
+  show DecrValue        = "<"
+  show MoveForth        = "+"
+  show MoveBack         = "-"
+  show Output           = "."
+  show Input            = ","
+  show ( Loop actions ) = "[" ++ concatMap show actions ++ "]"
+
+syntax :: [ Token ] -> Either [ ( [ Action ], [ Token ] ) ] [ Action ]
+syntax = runParser grammar
+  where
+    incrV, decrV, moveF, moveb, action :: Parser Token Action
+    incrV  = const IncrValue <$> term IncrB
+    decrV  = const DecrValue <$> term DecrB
+    moveF  = const MoveForth <$> term IncrP
+    moveb  = const MoveBack  <$> term DecrP
+    output = const Output    <$> term Out
+    input  = const Input     <$> term In
+    loop   = term Begin >> ( Loop <$> grammar ) << term End
+    action = msum [ incrV, decrV, moveF, moveb, output, input, loop ]
+
+    grammar :: Parser Token [ Action ]
+    grammar = many action
+
+-- Semantic.
+-- Actually is not mandatory, but it's possible to check the code
+-- has no meaning less actions, like ask for input twice or nested empty
+-- cycles etc.
+
+semantic :: Either [ ( [ Action ], [ Token ] ) ] [ Action ] -> [ Action ]
+semantic ( Left x )  = error ( show x )
+semantic ( Right l ) = l
+
+-- Interpret.
 
 type Memory = ( Seq.Seq Int, Int, Seq.Seq Int )
 
-type BF r = StateT Memory IO r
+interpret :: [ Action ] -> IO Memory
+interpret actions = evalStateT ( mapM_ eval actions >> get ) newMemory
+  where
+    newMemory :: Memory
+    newMemory = ( Seq.empty, 0, Seq.empty )
 
-newMemory :: Memory
-newMemory = ( Seq.empty, 0, Seq.empty )
-
-eval :: Token -> BF ()
-eval IncrB = modify ( second succ )
-eval DecrB = modify ( second pred )
-eval IncrP = do
-  ( p, v, n ) <- get
-  case Seq.viewl n of
-    x Seq.:< xs -> put ( p Seq.|> v, x, xs )
-    Seq.EmptyL  -> put ( p Seq.|> v, 0, n )
-eval DecrP = do
-  ( p, v, n ) <- get
-  case Seq.viewr p of
-    xs Seq.:> x -> put ( xs, x, v Seq.<| n )
-    Seq.EmptyR  -> put (  p, 0, v Seq.<| n )
-eval Output = do
-  ( _, v, _ ) <- get
-  lift ( putChar ( chr v ) )
-eval Input = do
-  v <- lift getChar
-  modify ( second ( const ( ord v ) ) )
+    eval :: Action -> StateT Memory IO ()
+    eval IncrValue = modify ( second succ )
+    eval DecrValue = modify ( second pred )
+    eval MoveForth = do
+      ( p, v, n ) <- get
+      case Seq.viewl n of
+        x Seq.:< xs -> put ( p Seq.|> v, x, xs )
+        Seq.EmptyL  -> put ( p Seq.|> v, 0, n )
+    eval MoveBack = do
+      ( p, v, n ) <- get
+      case Seq.viewr p of
+        xs Seq.:> x -> put ( xs, x, v Seq.<| n )
+        Seq.EmptyR  -> put (  p, 0, v Seq.<| n )
+    eval Output = do
+      ( _, v, _ ) <- get
+      lift ( putChar ( chr v ) )
+    eval Input = do
+      v <- lift getChar
+      modify ( second ( const ( ord v ) ) )
+    eval l@( Loop list ) = do
+      ( _, v, _ ) <- get
+      when ( v /= 0 )
+        ( mapM_ eval list >> eval l )
 
 main :: IO ()
 main = return ()
 
-first :: ( a -> a ) -> ( a, b, c ) -> ( a, b, c )
-first f ( a, b, c ) = ( f a, b, c )
-
 second :: ( b -> b ) -> ( a, b, c ) -> ( a, b, c )
 second f ( a, b, c ) = ( a, f b, c )
-
-third :: ( c -> c ) -> ( a, b, c ) -> ( a, b, c )
-third f ( a, b, c ) = ( a, b, f c )
